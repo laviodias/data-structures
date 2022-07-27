9783e6fafee0a752a9b758e6b6298971969fa934
Lavio <laviovale@gmail.com>
2022-07-26 21:21:57 -0300

    quicksort now has a random pivot

--- quicksort.js
+++ quicksort.js
@@ -2,11 +2,12 @@ function quickSort(array) {
   if (array.length < 2) {
     return array
   }
-  const pivot = array[0]
+  const randomNumber = Math.ceil(Math.random()*array.length-1)
+  const pivot = array[randomNumber]
   const left = array.filter(num => num < pivot)
   const right = array.filter(num => num > pivot)
 
   return quickSort(left).concat(pivot).concat(quickSort(right))
 }
 
-console.log(quickSort([3, 1, 9, 12, 4, 5, 0, 41, 11]))
+console.log(quickSort([3, 1, 9, 12, 4, 5, 0, 41, 11,2]))
